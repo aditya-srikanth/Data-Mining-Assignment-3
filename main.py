dafa4f0eb3192efc8c6dd08337b6b3374de642b9
aditya-srikanth <adityashrikanth14@gmail.com>
2019-04-11 03:55:31 +0530

    completed core point logic

---
+++ main.py
@@ -0,0 +1,107 @@
+import numpy as np 
+import os 
+from threading import Thread
+from multiprocessing import Pool,cpu_count
+from pprint import pprint
+
+from preprocess_dataset import process_dataset
+
+class DBSCAN:
+    def __init__(self,file_path,file_name,eps,minpts):
+        self.core_points = []
+        self.core_point_labels = []
+        self.core_points_index = []
+        self.n_threads = 5
+        self.eps = eps
+        self.minpts = minpts
+        self.features,self.labels = process_dataset(file_path,file_name)
+        # self.features,self.labels = self.features[:10000,:],self.labels[:10000]
+
+    def set_thread_count(self,n_threads):
+        self.n_threads = n_threads
+
+    def find_core_points(self,indices):
+        start_index = indices[0]
+        end_index = indices[1]
+        sample = self.features[start_index:end_index+1,:]
+        print('entered thread start index: ',start_index,' end index: ',end_index,' sample shape ',sample.shape)
+        distances = []
+        core_points = []
+        for point_index in range(sample.shape[0]):
+            # print('pointbreak ')
+            point = sample[point_index,:]
+            distances = np.sqrt(np.sum((self.features - point)**2,axis=1))
+            # print(np.argwhere(distances <= self.eps).shape[0])
+            # print('distances: ',distances)
+            if np.argwhere(distances <= self.eps).shape[0] >= self.minpts:
+                core_points.append(start_index + point_index)
+             
+        return core_points
+    
+    def find_border_points(self,indices):
+        start_index = indices[0]
+        end_index = indices[1]
+        print('entered thread',start_index,end_index)
+        distances = []
+        
+        sample = self.features[start_index:end_index+1,:]
+        
+        border_points = []
+
+        for point_index in range(sample.shape[0]):
+            # print('pointbreak ')
+            point = sample[point_index,:]
+            distances = np.sqrt(np.sum((self.features - point)**2,axis=1))
+            # print(np.argwhere(distances <= self.eps).shape[0])
+            # print('distances: ',distances)
+            if np.intersect1d(np.argwhere(distances <= self.eps),self.core_points_index):
+                border_points.append(start_index + point_index)
+
+        return border_points
+
+    def fit(self):
+        # get the number of training instances
+        N = self.features.shape[0]
+        # get the size of each input that the thread will take
+        size = (N)//self.n_threads
+        print('number of processors available: ',cpu_count())
+        # create the thread pool
+        thread_pool_for_core_points = Pool(processes = self.n_threads)
+        # thread_pool_for_border_points = Pool(processes = self.n_threads)
+
+        indices = []
+        for start_index in range(0,N,size):
+            if start_index + size < N:
+                end_index = start_index + size - 1
+                print('processsing: ',start_index,end_index)
+                indices.append((start_index,end_index))
+            else:
+                end_index = start_index + (N - start_index) - 1
+                print('final iteration: ',start_index,end_index)
+                indices.append((start_index,end_index))
+        
+        results = thread_pool_for_core_points.map(self.find_core_points,indices)
+
+        thread_pool_for_core_points.close()
+        thread_pool_for_core_points.join()
+        
+        
+
+        for result in results:
+            for index in result:
+                self.core_points_index.append(index)
+        
+        self.core_points = self.features[index,:]
+        self.core_point_labels = self.labels[index,:]
+        
+        print(self.core_points.shape)
+
+        # np.delete(self.features,self.core_points_index,axis=0)
+        # np.delete(self.labels,self.core_points_index,axis=0)
+
+
+        # self.core_points = np.array(self.core_points)
+
+if __name__ == "__main__":
+    test = DBSCAN("creditcardfraud","creditcard.csv",10,200)
+    test.fit()
--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -4,18 +4,27 @@ import pickle
 import pandas as pd
 
 def process_dataset(file_path,file_name):
-    if not os.path.isdir(file_name+"processed_data"):
-        with open(file_path,"rb") as f:
+    if not os.path.isdir("processed_data"):
+        with open(file_path+'/'+file_name,"rb") as f:
             data = pd.read_csv(f).values
-            features = data[:,:-1]
-            labels = data[:,-1]
+        features = data[:,:-1]
+        labels = np.reshape(data[:,-1],(data.shape[0],1))
+        
+        # feature scaling
+        features = (features - np.mean(features,axis=0))/np.std(features,axis=0)
+        
+        os.mkdir("processed_data")
+
+        with open("./processed_data/features.txt",'wb') as f:
+            pickle.dump(features,f)
+        with open("./processed_data/labels.txt",'wb') as f:
+            pickle.dump(labels,f)
 
         
     else:
-        with open(file_name+"processed_data/features.txt") as f:
+        with open("processed_data/features.txt",'rb') as f:
             features = pickle.load(f)
-        with open(file_name+"processed_data/labels.txt") as f:
+        with open("processed_data/labels.txt",'rb') as f:
             labels = pickle.load(f)
-    
-        return features,labels
-process_dataset()
+        
+    return features,labels
