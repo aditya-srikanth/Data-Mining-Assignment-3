730680b8050147bdb97999852a6a0f21f3b71474
kvr-aditya577 <f20170245@hyderabad.bits-pilani.ac.in>
2019-04-14 23:30:52 +0530

    handling memory issues

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 import numpy as np 
 import os 
-from threading import Thread
-from multiprocessing import Pool,cpu_count
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+from multiprocessing import get_context
+import SharedArray as sa
 from pprint import pprint
+import gc
 
+import visualization 
 from preprocess_dataset import process_dataset
+
+def find_core_points(indices):
+        features = sa.attach("shm://features")
+        print('features: \n',features.shape)
+        minpts = 10
+        eps = 1
+        start_index = indices[0]
+        end_index = indices[1]
+        sample = features[start_index:end_index+1,:]
+        print('entered thread start index: ',start_index,' end index: ',end_index,' sample shape ',sample.shape)
+        distances = []
+        core_points = []
+        nearest_neighbours = {}
+        for point_index in range(sample.shape[0]):
+            if point_index % 1000 == 0:
+                print('collected garbage')
+                gc.collect()
+            point = sample[point_index,:]
+            distances = np.sqrt(np.sum((features - point)**2,axis=1))
+            candidates = np.argwhere(distances <= eps)
+            if candidates.shape[0] > minpts:
+                core_points.append(start_index + point_index)
+                nearest_neighbours[start_index+point_index] = set(list(candidates.flatten()))
+        print('exiting: ',start_index,end_index,'\n\n')
+        sa.delete("shm://features")
+        return core_points,nearest_neighbours
+
 
 class DBSCAN:
     def __init__(self,file_path,file_name,eps=1,minpts=1):
@@ -14,55 +45,74 @@ class DBSCAN:
         self.border_points_index = []
         self.border_points = []
         self.border_point_labels = []
+        self.noise_points = []
         self.nearest_neighbours = {}
-        self.n_threads = 5
+        self.n_threads = cpu_count()
         self.eps = eps
         self.minpts = minpts
         self.features,self.labels = process_dataset(file_path,file_name)
-        self.features,self.labels = self.features[:2000,:],self.labels[:2000]
+        self.features,self.labels = self.features[:200000,:],self.labels[:200000]
+        sa.delete("shm://features") ## TODO send this down
+        self.shared_memory = sa.create("shm://features",self.features.shape)
+
+        # copy the array into the shared memory
+        for row_index in range(self.features.shape[0]):
+            for point_index in range(self.features.shape[1]):
+                self.shared_memory[row_index,point_index] = self.features[row_index,point_index]
         self.clusters = []
 
     def set_thread_count(self,n_threads):
         self.n_threads = n_threads
 
-    def find_core_points(self,indices):
-        start_index = indices[0]
-        end_index = indices[1]
-        sample = self.features[start_index:end_index+1,:]
-        print('entered thread start index: ',start_index,' end index: ',end_index,' sample shape ',sample.shape)
-        distances = []
-        core_points = []
-        nearest_neighbours = {}
-        for point_index in range(sample.shape[0]):
-            point = sample[point_index,:]
-            distances = np.sqrt(np.sum((self.features - point)**2,axis=1))
-            candidates = np.argwhere(distances <= self.eps)
-            if candidates.shape[0] > self.minpts:
-                core_points.append(start_index + point_index)
-                nearest_neighbours[start_index+point_index] = set(list(candidates.flatten()))
-             
-        return core_points,nearest_neighbours
+    # def find_core_points(self,indices,eps,minpts):
+    #     features = sa.attach("shm://features")
+    #     print('features: \n',features.shape)
+
+    #     start_index = indices[0]
+    #     end_index = indices[1]
+    #     sample = features[start_index:end_index+1,:]
+    #     print('entered thread start index: ',start_index,' end index: ',end_index,' sample shape ',sample.shape)
+    #     distances = []
+    #     core_points = []
+    #     nearest_neighbours = {}
+    #     for point_index in range(sample.shape[0]):
+    #         if point_index % 100 == 0:
+    #             print('index: ',point_index)
+    #             gc.collect()
+    #             print('collected garbage')
+    #         point = sample[point_index,:]
+    #         distances = np.sqrt(np.sum((features - point)**2,axis=1))
+    #         candidates = np.argwhere(distances <= eps)
+    #         if candidates.shape[0] > minpts:
+    #             core_points.append(start_index + point_index)
+    #             nearest_neighbours[start_index+point_index] = set(list(candidates.flatten()))
+    #     print('exiting: ',start_index,end_index,'\n\n')
+    #     return core_points,nearest_neighbours
     
     def find_border_points(self,indices):
+        features = sa.attach("shm://features")
         start_index = indices[0]
         end_index = indices[1]
         print('entered thread',start_index,end_index)
         distances = []
         
-        sample = self.features[start_index:end_index+1,:]
+        sample = features[start_index:end_index+1,:]
         
         border_points = []
 
         for point_index in range(sample.shape[0]):
+            if point_index % 1000 == 0:
+                gc.collect()
+                print("collecting")
             point = sample[point_index,:]
-            distances = np.sqrt(np.sum((self.features - point)**2,axis=1))
+            distances = np.sqrt(np.sum((features - point)**2,axis=1))
 
             candidates = np.argwhere(distances <= self.eps) + start_index
 
             # if it is not a core point and is in the vicinity of a core point
             if np.intersect1d(candidates,self.core_points_index).shape[0] >= 1 and not (start_index + point_index) in self.core_points_index:
                 border_points.append(start_index + point_index)
-
+        print('exiting border finding: ',start_index,end_index,end_index - start_index + 1,'\n\n')
         return border_points
 
     def fit(self):
@@ -72,25 +122,24 @@ class DBSCAN:
         size = (N)//self.n_threads
         print('number of processors available: ',cpu_count())
 
-        # create the thread pool for core and border points
-        thread_pool_for_core_points = Pool(processes = self.n_threads)
-        thread_pool_for_border_points = Pool(processes = self.n_threads)
+        with get_context("spawn").Pool(processes=self.n_threads,maxtasksperchild=1) as thread_pool_for_core_points:
+            # create the thread pool for core and border points
+            core_indices = []
+            for start_index in range(0,N,size):
+                if start_index + size < N:
+                    end_index = start_index + size - 1
+                    # print('processsing: ',start_index,end_index)
+                    core_indices.append((start_index,end_index))
+                else:
+                    end_index = start_index + (N - start_index) - 1
+                    # print('final iteration: ',start_index,end_index)
+                    core_indices.append((start_index,end_index))
+                    # core_indices.append((eps,minpts))
+            
+            core_results = thread_pool_for_core_points.map(find_core_points,core_indices)
 
-        core_indices = []
-        for start_index in range(0,N,size):
-            if start_index + size < N:
-                end_index = start_index + size - 1
-                # print('processsing: ',start_index,end_index)
-                core_indices.append((start_index,end_index))
-            else:
-                end_index = start_index + (N - start_index) - 1
-                # print('final iteration: ',start_index,end_index)
-                core_indices.append((start_index,end_index))
-        
-        core_results = thread_pool_for_core_points.map(self.find_core_points,core_indices)
-
-        thread_pool_for_core_points.close()
-        thread_pool_for_core_points.join()
+            thread_pool_for_core_points.close()
+            thread_pool_for_core_points.join()
 
         for result in core_results:
             self.nearest_neighbours.update(result[1])
@@ -100,19 +149,20 @@ class DBSCAN:
         self.core_points = self.features[self.core_points_index,:]
         self.core_point_labels = list(self.labels[self.core_points_index,:])
         
-        border_indices = []
-        for start_index in range(0,N,size):
-            if start_index + size < N:
-                end_index = start_index + size - 1
-                border_indices.append((start_index,end_index))
-            else:
-                end_index = start_index + (N - start_index) - 1
-                border_indices.append((start_index,end_index))
-        
-        border_results = thread_pool_for_border_points.map(self.find_border_points,border_indices)
+        with get_context("spawn").Pool(processes=self.n_threads,maxtasksperchild=1) as thread_pool_for_border_points:
+            border_indices = []
+            for start_index in range(0,N,size):
+                if start_index + size < N:
+                    end_index = start_index + size - 1
+                    border_indices.append((start_index,end_index))
+                else:
+                    end_index = start_index + (N - start_index) - 1
+                    border_indices.append((start_index,end_index))
+            
+            border_results = thread_pool_for_border_points.map(self.find_border_points,border_indices)
 
-        thread_pool_for_border_points.close()
-        thread_pool_for_border_points.join()
+            thread_pool_for_border_points.close()
+            thread_pool_for_border_points.join()
 
 
         for result in border_results:
@@ -152,14 +202,36 @@ class DBSCAN:
                     queue += list(self.nearest_neighbours[point])
                     # print('node: ',node)
                     visited[node] = True
-            print('cluster: ',cluster)
             if len(cluster) > 0:
                 clusters.append(cluster)
         
-        print(len(clusters))
-        print(clusters)
+        print('number of clusters: ',len(clusters))
+        # print('clusters are:')
+        # for cluster in clusters:
+        #     print(cluster)
         self.clusters = clusters
+        cluster_points = set(self.border_points_index + self.core_points_index)
+        all_points = set([i for i in range(self.features.shape[0])])
+        self.noise_index = all_points.difference(cluster_points)
+        self.noise_points = [self.features[i,:] for i in self.noise_index]
+        return self.noise_points
+    
+    
+    def plot(self):
+        visuals = visualization.Visualization()
+        for point in self.noise_points:
+            visuals.OUTLIERS.append(visuals.dimension_reduction(point))
+        for point in self.core_points:
+            visuals.NON_OUTLIERS.append(visuals.dimension_reduction(point))
+        for point in self.border_points:
+            visuals.NON_OUTLIERS.append(visuals.dimension_reduction(point)) 
+        visuals.outlier_plot()           
 
 if __name__ == "__main__":
-    test = DBSCAN("creditcardfraud","creditcard.csv",eps=2,minpts=20)
-    test.fit()
+    test = DBSCAN("creditcardfraud","creditcard.csv",eps=10,minpts=10)
+    outliers = test.fit()
+    print('outliers are: ',len(outliers))
+    with open('noise.txt','w') as f:
+        for noise_point in outliers:
+            print(noise_point,file=f)
+    # test.plot()
